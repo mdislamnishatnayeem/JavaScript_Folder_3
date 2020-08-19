d651d1f420f4c66c029165a3afe48d5a977a580c
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-19 13:44:33 +0600

    JSCODEME
    I HAVE TAKE THIS CODE FROME A BOOK programming bolod to boss,js code

---
+++ nayeem.js
@@ -0,0 +1,8 @@
+function nayeem(height,weight,eyecolor,age){
+	this.nayeemheight=height;
+	this.hisweight=weight;
+	this.hiseyecolor=eyecolor;
+	this.hisage=age;
+}
+var nayeemvai=new nayeem(5.6,56,"black",17);
+console.log(nayeemvai);
