7b960a92decf11cd0c85463cd5dec26271b95cfa
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-20 17:10:07 +0600

    Hashtable code
    Book:programming bolod to boss,jhankar mahbub

---
+++ Hashtable.js
@@ -0,0 +1,17 @@
+var roll={};
+var student={
+  1:"Md.Nayeem Islam",
+  2:"kayimm",
+  3:"Rohim",
+  4:"Korim",
+  5:"Morim",
+  6:"Dhorim",
+  7:"Amina",
+  8:"jamina",
+  9:"pabina",
+  10:"Nishat"
+  
+  }
+
+roll["all"]=student;
+console.log(roll);
