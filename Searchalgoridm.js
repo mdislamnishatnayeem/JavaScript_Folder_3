fd66c0a377e51d4129f626bcb472c46073262a59
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-20 16:10:38 +0600

    Programming bolod to boss
    I have take this code frome book. Of jhankar mahbub (programming bolod to boss).and some are modified by me, js code

---
+++ Searchalgoridm.js
@@ -0,0 +1,23 @@
+//a code of js frome book programming
+// bolod to boss...modified by me 
+
+var chokirtola=["juta","charger","bell",
+                "trang"," jharu"];
+for(var i=0;i<chokirtola.length;i++){
+  var jinis=chokirtola[i];
+  if(jinis=="charger"){
+   console.log("Charger paici");
+   console.log(i);
+   console.log(chokirtola[i]);
+   console.log(jinis);
+    console.log(chokirtola.
+                indexOf(jinis));
+    
+   console.log(chokirtola.indexOf(
+     "charger"));
+    
+    
+   console.log(chokirtola.indexOf
+               (" pent"));
+    }
+  }
