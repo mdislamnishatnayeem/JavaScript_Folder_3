95078c16df39052c7f5a188d10dde08c0d6de696
mdislamnishatnayeem <69844284+mdislamnishatnayeem@users.noreply.github.com>
2020-08-20 16:45:57 +0600

    Hashtable and dictionarybased bolod to boss

---
+++ Nationalidcard.js
@@ -0,0 +1,13 @@
+//dictionary and hashtable
+
+var Bangladesh ={};
+
+var nayeem={
+  name:"Md.Nayeem Islam Nishat",
+  Fathername:"Md.Razzak Ali",
+  Mothername:"Mst.Fatema Zinna",
+  Address:"Bangladesh,Dinajpur,Newtown 7",
+  DateofBirth:"2002,18th November"
+  }
+Bangladesh[01728485133]=nayeem;
+console.log(Bangladesh
